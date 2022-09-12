f2dc249d915a9e872e082e47b94a63b00ac9e112
Matheus Popolin <matheuspopolin@hotmail.com>
2022-09-12 08:52:07 -0300

    Primeira versão

--- products.js
+++ products.js
@@ -10,21 +10,21 @@ const products = [
     title: "Banana",
     price: 5.69,
     category: "Frutas",
-    image: "./img/products/product_2.svg",
+    image: "/img/products/product_2.svg",
     imageDescription: "",
   },
   {
     title: "Mamão",
     price: 4.99,
     category: "Frutas",
-    image: "./img/products/product_3.svg",
+    image: "/img/products/product_3.svg",
     imageDescription: "",
   },
   {
     title: "Maçã",
-    price: 9.2,
+    price: 9.20,
     category: "Frutas",
-    image: "./img/products/product_4.svg",
+    image: "/img/products/product_4.svg",
     imageDescription: "",
   },
   {
@@ -38,7 +38,7 @@ const products = [
     title: "Vinho",
     price: 8.99,
     category: "Bebidas",
-    image: "./img/products/product_6.svg",
+    image: "/img/products/product_6.svg",
     imageDescription: "",
   },
   {
@@ -52,7 +52,7 @@ const products = [
     title: "Água de coco",
     price: 8.99,
     category: "Bebidas",
-    image: "./img/products/product_8.svg",
+    image: "/img/products/product_8.svg",
     imageDescription: "",
   },
 
@@ -60,7 +60,7 @@ const products = [
     title: "Sabonete",
     price: 8.99,
     category: "Higiene",
-    image: "./img/products/product_9.svg",
+    image: "/img/products/product_9.svg",
     imageDescription: "",
   },
 
@@ -68,7 +68,7 @@ const products = [
     title: "Detergente",
     price: 8.99,
     category: "Higiene",
-    image: "./img/products/product_10.svg",
+    image: "/img/products/product_10.svg",
     imageDescription: "",
   },
 
@@ -76,7 +76,7 @@ const products = [
     title: "Limpa superfícies",
     price: 8.99,
     category: "Higiene",
-    image: "./img/products/product_11.svg",
+    image: "/img/products/product_11.svg",
     imageDescription: "",
   },
 
@@ -88,3 +88,61 @@ const products = [
     imageDescription: "",
   },
 ];
+
+let listFrutas  = [];
+let listBebidas = [];
+let listHigiene = [];
+
+function separateItens(lista){
+    for(let i=0; i<lista.length; i++){
+        if(lista[i].category=="Frutas"){
+            listFrutas.push(lista[i]);
+        } else if(lista[i].category=="Bebidas"){
+            listBebidas.push(lista[i]);
+        } else if(lista[i].category=="Higiene"){
+            listHigiene.push(lista[i]);
+        }
+    }
+}
+
+separateItens(products);
+
+function listaItens(lista){
+  let tagSection = document.createElement("section");
+  tagSection.classList.add("products-section");
+  tagSection.innerHTML = `<h1>${lista[0].category}</h1>`;
+  
+  let tagDiv  = document.createElement("div");
+  tagDiv.classList.add("products-content")
+
+  let tagUl   = document.createElement("ul");
+
+  for(let i=0; i<lista.length; i++){
+    let tagLi = document.createElement("li");
+    tagLi.classList.add("product");
+    tagLi.innerHTML = 
+      `<img src="/img/products/no-img.svg"
+        alt="Imagem de ${lista[i].title}"
+        class="product-img"
+      />
+      <div class="product-div">
+        <h1 class="product-title">${lista[i].title}</h1>
+        <h5 class="product-category">${lista[i].category}</h5>
+        <strong class="product-price">R$ ${lista[i].price}</strong>
+      </div>`;
+    if(lista[i].image){
+      tagLi.querySelector("img").src = lista[i].image
+    }
+    tagUl.appendChild(tagLi);   
+  }
+  tagDiv.appendChild(tagUl);
+  tagSection.appendChild(tagDiv);
+
+  tagMain = document.querySelector("main")
+  tagMain.appendChild(tagSection);
+}
+
+listaItens(listFrutas);
+listaItens(listBebidas);
+listaItens(listHigiene);
+
